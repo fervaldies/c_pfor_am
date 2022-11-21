7a368d3c882c059c8d58f17126f62b03dc8b02b5
Fernando Valiente Dies <118708559+fervaldies@users.noreply.github.com>
2022-11-21 17:50:56 +1100

    UpdateCoordinates

--- src/materials/VelocityEllipsoidHeatSource.C
+++ src/materials/VelocityEllipsoidHeatSource.C
@@ -82,9 +82,9 @@ VelocityEllipsoidHeatSource::initQpStatefulProperties()
 void
 VelocityEllipsoidHeatSource::computeQpProperties()
 {
-  _x_coord = _init_x_coords[_n_track];
-  _y_coord = _init_y_coords[_n_track];
-  _z_coord = _init_z_coords[_n_track];
+  _x_coord = _init_x_coords[0];
+  _y_coord = _init_y_coords[0];
+  _z_coord = _init_z_coords[0];
 
   const Real & x = _q_point[_qp](0);
   const Real & y = _q_point[_qp](1);
