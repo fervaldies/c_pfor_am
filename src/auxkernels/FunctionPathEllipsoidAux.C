61c61f8bfb13f64e0bcfc0d057cf96020eda8fe1
Fernando Valiente Dies <118708559+fervaldies@users.noreply.github.com>
2022-12-01 13:27:35 +1100

    Update

--- src/auxkernels/FunctionPathEllipsoidAux.C
+++ src/auxkernels/FunctionPathEllipsoidAux.C
@@ -73,7 +73,7 @@ FunctionPathEllipsoidAux::FunctionPathEllipsoidAux(const InputParameters & param
 }
 
 void
-VelocityEllipsoidHeatSource::initQpStatefulProperties()
+FunctionPathEllipsoidAux::initQpStatefulProperties()
 {
   // Initialize coordinates of the heat source
   _t_scan = _t;
